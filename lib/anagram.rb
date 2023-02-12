ed56b879c0f8ee5a84577ae4e7430f854197a24a
Leon simintei <mallfs@icloud.com>
2023-02-12 23:13:35 +0300

    assignment complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(list)
+        list.filter do |my_word|
+            my_word.chars.sort == @word.chars.sort
+        end
+    end
+end
